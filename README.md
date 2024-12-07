75971eda007fda69fa93a611329ff807ae93f52c
TiieuTiien <phamquoctien1903@gmail.com>
2024-12-07 19:22:53 +0700

    Update README.md and split data by group

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# population-distribution-prediction
+# Analysis and Prediction of Population Distribution Based on Linear Regression Model.
+
+This project involves analyzing a dataset of global population distribution from 1950 to 2021, utilizing a linear regression model for predictive insights. The dataset contains various numeric fields, and the analysis includes calculating essential statistical measures such as minimum, maximum, mean, standard deviation, and quantiles for the cleaned data. The project also involves checking for inconsistencies between 'MidPeriod' and 'Time' fields to ensure data integrity. By focusing on numeric data and handling missing values attentively, this analysis provides a comprehensive overview of the population distribution trends over the years. The insights gained from the statistics and regression modeling can be instrumental in understanding historical changes and forecasting future demographic distributions.
+
+
+
--- sample_data.py
+++ sample_data.py
@@ -2,19 +2,21 @@ import pandas as pd
 import glob
 import os
 
-# Đường dẫn đến thư mục chứa các thư mục con
-root_path = './archive'
-output_dir = 'sample'
+# Đường dẫn đến thư mục chứa các file CSV
+root_path = './data'  # Relative path to the 'data' directory
+output_dir = './data_samples/sample'  # Relative path for output directory
+
+# Print the output directory
+print(f'The output directory is set to: {output_dir}')
 
 # Tạo thư mục 'sample' nếu chưa tồn tại
 os.makedirs(output_dir, exist_ok=True)
 
-# Duyệt qua các thư mục con và lấy file CSV
-for subdir in glob.glob(os.path.join(root_path, '*/')):  # Lấy các thư mục con
-    csv_files = glob.glob(os.path.join(subdir, '*.csv'))
-    for file in csv_files:
-        df = pd.read_csv(file)
-        sample = df.head(100)
-        output_path = os.path.join(output_dir, f'sample_{os.path.basename(file)}')
-        sample.to_csv(output_path, index=False)  # Lưu file mẫu vào thư mục 'sample'
-        print(f'Sample từ {file} đã được lưu vào {output_path}.')
+# Lấy các file CSV trực tiếp trong thư mục ./data
+csv_files = glob.glob(os.path.join(root_path, '*.csv'))
+for file in csv_files:
+    df = pd.read_csv(file, low_memory=False)
+    sample = df.head(100)
+    output_path = os.path.join(output_dir, f'sample_{os.path.basename(file)}')
+    sample.to_csv(output_path, index=False)  # Lưu file mẫu vào thư mục 'sample'
+    print(f'Sample từ {file} đã được lưu vào {output_path}.')
